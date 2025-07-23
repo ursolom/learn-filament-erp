ab7781eadf2df2b83ee46458463e62bd2c1ad35e
soliman <soft9.me@gmail.com>
2025-07-23 19:00:54 +0300

    end and lts go to muilt-tenant

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Models\Team;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -66,6 +67,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
+            ->tenant(Team::class)
             ->spa()
             ->sidebarCollapsibleOnDesktop();
     }
